26d3fd6cdbdb09cbbaba4ec82d5af4d206817485
akdiem <alexandra.diem@gmail.com>
2018-09-26 10:33:08 +0200

    Setup.py file appropriate for Python 3

--- setup.py
+++ setup.py
@@ -1,59 +1,18 @@
-import os
-import sys
-import subprocess
-import string
-import platform
+from distutils.core import setup
 
-from setuptools import setup, find_packages, Command
-
-# Version number
-major = 0
-minor = 1
-
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Get the long description from the README file
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-
-if platform.system() == "Windows" or "bdist_wininst" in sys.argv:
-    # In the Windows command prompt we can't execute Python scripts
-    # without a .py extension. A solution is to create batch files
-    # that runs the different scripts.
-    batch_files = []
-    for script in scripts:
-        batch_file = script + ".bat"
-        f = open(batch_file, "w")
-        f.write('python3 "%%~dp0\%s" %%*\n' % os.path.split(script)[1])
-        f.close()
-        batch_files.append(batch_file)
-    scripts.extend(batch_files)
-
-
-if on_rtd:
-    REQUIREMENTS = []
-else:
-    REQUIREMENTS = [
+setup(
+    name='poroelastic',
+    version='0.1.0',
+    author='Alexandra K. Diem',
+    author_email='alexandra@simula.no',
+    packages=['poroelastic', 'test'],
+    url='https://github.com/akdiem/poroelastic',
+    license='LICENSE',
+    description='A Python solver for poroelastic models using DOLFIN',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "dolfin == 2017.2.0",
         "numpy",
-        "scipy",
-        "matplotlib",
-        "dolfin",
-    ]
-
-
-setup(name = "poroelasticity",
-      version = "{0}.{1}".format(major, minor),
-      description = """
-      A poroelasticity solver using Fenics/Dolfin.
-      """,
-      url='https://github.com/akdiem/poroelasticity',
-      author = "Alexandra Diem",
-      author_email = "alexandra@simula.no",
-      license="BSD version 3",
-      install_requires=REQUIREMENTS,
-      packages = ["poroelasticity"],
-      package_dir = {"poroelasticity": "poroelasticity"},
-      )
+        "h5py"
+    ],
+)
