d59224bd5b469ee7f6062de9281efca33f82a3f7
akdiem <alexandra.diem@gmail.com>
2018-12-25 10:20:29 +0100

    Material models now use a common constitutive_law() function using **kwargs

--- poroelastic/material_models.py
+++ poroelastic/material_models.py
@@ -16,7 +16,18 @@ class IsotropicExponentialFormMaterial(object):
         self.Qi3 = Constant(1.0)
 
 
-    def constitutive_law(self, I1, I2, J, M, rho):
+    def constitutive_law(self, F, **kwargs):
+        # kwargs
+        M = kwargs["M"]
+        rho = kwargs["rho"]
+
+        # kinematic variables
+        J = variable(det(F))
+        C = variable(F.T*F)
+        I1 = variable(J**(-2/3) * tr(C))
+        I2 = variable(J**(-4/3) * 0.5 * (tr(C)**2 - tr(C*C)))
+
+        # constitutive law
         Psi = self.a * (exp(self.D1 * (I1 * (1 + self.Qi1*(M/rho)) - 3)\
             + self.D2 * (I2 *(1 + self.Qi2*(M/rho)) - 3)\
             + self.D3 * ((J-1)**2 + self.Qi3*(M/rho)**2)) - 1)
@@ -33,6 +44,11 @@ class NeoHookeanMaterial(object):
         self.lm = Constant(E*nu/((1 + nu)*(1 - 2*nu)))
 
 
-    def constitutive_law(self, Ic, J):
+    def constitutive_law(self, F, **kwargs):
+        # kinematic variables
+        J = variable(det(F))
+        C = variable(F.T*F)
+        Ic = variable(J**(-2/3) * tr(C))
+
         Psi = (self.mu/2)*(Ic - 3) - self.mu*ln(J) + (self.lm/2)*(ln(J))**2
         return Psi
