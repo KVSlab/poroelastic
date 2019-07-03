37e520ba27fbc7413748ed33cc86844753ae7251
lisapankewitz <lisa.pankewitz@yahoo.de>
2019-07-03 09:33:17 -0400

    Added test_periodic() - new function

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,12 @@
 from poroelastic.utils import *
+import numpy as np
 
 def test_periodic():
-    assert periodic(5.0, 1.0) == 1.0
-    assert periodic(5.0, 2.0) == 1.0
+    t = 5
+    T = 1
+    a = list(range(0,t,T))
+    if a[0] > abs(a[1]-a[0]):
+        c = a[0]
+    else:
+        c = a[1]
+    assert periodic(t,T) == c
