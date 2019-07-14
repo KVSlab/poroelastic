be3a890b2d2397e6400cd2d3c9674f0bf7ead427
lisapankewitz <lisa.pankewitz@yahoo.de>
2019-07-14 07:13:27 -0400

    Not  executed by pytest yet. Will fix. testing paramters in init function of material_models module.

--- test/test_material_models.py
+++ test/test_material_models.py
@@ -1,2 +1,17 @@
+import sys
+# sys.path.insert(0, './poroelastic/')
+import pytest
+import dolfin as df
+from poroelastic.utils import *
+from poroelastic.param_parser import *
 from poroelastic.material_models import *
 import numpy as np
+
+@pytest.fixture
+def test_init(material_models, param):
+    a = param
+    #material_models.IsotropicExponentialFormMaterial(material_models, param)
+    assert material_models.IsotropicExponentialFormMaterial.a == 1.0
+
+def test_test():
+    print("I am a test to test the test.")
