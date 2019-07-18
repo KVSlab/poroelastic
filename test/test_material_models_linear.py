1787d6bfc82f6a920c895f1817e175ce4da9a6c3
lisapankewitz <lisa.pankewitz@yahoo.de>
2019-07-18 10:47:37 -0400

    Added values for dict without unit be substituting them using regular expression.  Now keys are returned in lower keys, needs to be fixed.

--- test/test_material_models_linear.py
+++ test/test_material_models_linear.py
@@ -1,10 +1,12 @@
 import sys
+import re
 # sys.path.insert(0, './poroelastic/')
 import pytest
 import dolfin as df
-from poroelastic import *
-#from poroelastic.param_parser import *
-#from poroelastic.material_models import *
+#from poroelastic import *
+from poroelastic.utils import *
+from poroelastic.param_parser import *
+from poroelastic.material_models import *
 import numpy as np
 import configparser
 
@@ -26,16 +28,29 @@ def test_param_file_linearporoelastic():
 def param_linearporo(test_param_file_linearporoelastic):
     configure = configparser.ConfigParser()
     configure.read('/tmp/param_linearporo.cfg')
+    section = 'Material'
+    options = configure.items(section)
     param_linearporo = {}
-
-    param_linearporo["kappa0"] = configure.get('Material','kappa0')
-    param_linearporo["kappa1"] = configure.get('Material','kappa1')
-    param_linearporo["kappa2"] = configure.get('Material','kappa2')
-    param_linearporo["K"] = configure.get('Material','K')
-    param_linearporo["M"] = configure.get('Material','M')
-    param_linearporo["b"] = configure.get('Material','b')
-
+    for key, value in options:
+        if '*' in value:
+            value = re.sub(r" * .*", "", value, flags=re.I)
+            param_linearporo[key] = value
+        else:
+            param_linearporo[key] = value
+    print(param_linearporo)
     return param_linearporo
+
+        #print(key)
+        #param_linearporo[key] = value
+'''
+    param_linearporo["kappa0"] = configure.getfloat('Material','kappa0')
+    param_linearporo["kappa1"] = configure.getfloat('kappa1')
+    param_linearporo["kappa2"] = configure.getfloat('kappa2')
+    param_linearporo["K"] = configure.getfloat('K')
+    param_linearporo["M"] = configure.getfloat('M')
+    param_linearporo["b"] = configure.getfloat('b')
+'''
+    #return param_linearporo
 
 @pytest.fixture
 def linearporoelasticmaterial(param_linearporo):
