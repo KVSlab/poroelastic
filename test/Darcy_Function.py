087d96e620cdf770d02f5ec7ba75965626cd4ecd
lisapankewitz <lisa.pankewitz@yahoo.de>
2019-08-06 10:24:45 +0200

    Set FLuid boundary conditions to pressure limits output of gluielastic problem.

--- test/Darcy_Function.py
+++ test/Darcy_Function.py
@@ -12,14 +12,9 @@ def Darcy(mesh):
 
     # Create mesh values to iterate over and define function space
     P = FunctionSpace(mesh, 'P', 1) # Pressure so take P
-    #calculate exact solution
-    #p_e = Expression("1 - x[0]*x[0]", degree = 2)
-
 
     # define Constant: here Kappa for permeability
     kappa = Constant(1e-7)
-    #define phi
-    #phi = Constant(0.3)
 
     # Define boundary condition
     def left(x, on_boundary):
@@ -28,8 +23,8 @@ def Darcy(mesh):
     def right(x, on_boundary):
         return on_boundary and near(x[0], 1.0)
 
-    bcl = DirichletBC(P, Constant(1e-3), left)
-    bcr = DirichletBC(P, Constant(0.88), right)
+    bcl = DirichletBC(P, Constant(1.09), left)
+    bcr = DirichletBC(P, Constant(2.46), right)
     bcs = [bcl, bcr]
 
     #Define variational problem
@@ -44,33 +39,4 @@ def Darcy(mesh):
     p_sol = Function(P)
     solve(a == L, p_sol, bcs)
 
-    #return for count!
     return p_sol
-
-#plot solution
-#p_sol = Darcy(mesh)
-# plt.figure()
-# fig = plot(p_sol)
-# plt.colorbar(fig)
-# plt.savefig("darcy.png")
-# #Postprocessing Velocity
-# kappa = Constant(1e-7)
-# phi = Constant(0.3)
-# elem_v = VectorElement("DG", triangle, 0)
-# W_v = FunctionSpace(mesh, elem_v)
-# grad_p = project(grad(p_sol), W_v)
-# vel_f = - kappa * grad_p / phi
-# plt.figure()
-# fig = plot(vel_f)
-# plt.colorbar(fig)
-# plt.savefig("vel_f")
-#
-#
-# elem_viz = VectorElement("CG", triangle, 1)
-# W_viz = FunctionSpace(mesh, elem_viz)
-# v_viz = project(vel_f, W_viz)
-# # Save solution to file in VTK format for paraview
-# v_file = File('Darcy_vel.pvd')
-# p_file = File('Darcy_p.pvd')
-# v_file << v_viz
-# p_file << p_sol
